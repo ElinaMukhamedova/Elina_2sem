04d838b677ba61f89cb8ea3415438ac4eb5f2cc0
Elina <elinamuhamedova13@gmail.com>
2022-02-03 20:59:41 +0300

    ex6 + folder

---
+++ 3 Feb/ex3.cpp
@@ -0,0 +1,23 @@
+//
+//  ex3.cpp
+//  
+//
+//  Created by Elina Mukhamedova on 03.02.2022.
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    int ans = 1;
+    while (n > 0){
+        ans *= n;
+        n -= 1;
+    }
+    std::cout << ans << '\n';
+    return 0;
+}
+
---
+++ 3 Feb/ex6.cpp
@@ -0,0 +1,51 @@
+//
+//  ex6.cpp
+//  
+//
+//  Created by Elina Mukhamedova on 03.02.2022.
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int gcd(int a, int b) {
+    int c = 0;
+    while (a % b != 0) {
+        c = a - b;
+        if (b >= c) {
+            a = b;
+            b = c;
+        }
+        else {
+            a = c;
+        }
+    }
+    return b;
+}
+
+int lcm(int a, int b) {
+    int res = a;
+    while (res % b != 0) {
+        res += a;
+    }
+    return res;
+}
+
+int main() {
+    int n = 0;
+    int m = 0;
+    int a = 0;
+    int b = 0;
+    std::cin >> n >> m;
+    if (n >= m){
+        a = n;
+        b = m;
+    }
+    else {
+        a = m;
+        b = n;
+    }
+    std::cout << "gcd = " << gcd(a, b) << ", lcm = " << lcm(a, b) << "\n";
+    return 0;
+}
