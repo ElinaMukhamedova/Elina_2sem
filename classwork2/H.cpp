30cc9d073603e5c7ca03cdaa7c56ec2afe70acc7
Elina <elinamuhamedova13@gmail.com>
2022-02-10 21:49:31 +0300

    H

--- classwork2/H.cpp
+++ classwork2/H.cpp
@@ -7,29 +7,29 @@
 
 #include <iostream>
 
+int power(int a, int b) {
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+        res *= a;
+    return res;
+}
+
 int main() {
     int n;
-    //std::cin >> n;
-    //int a = 0;
-    //int mult = 1;
-    //int inp = -1;
-    //for (int i = 1; i <= n; i++) {
-    //    std::cin >> inp;
-    //    a += mult * inp;
-    //    mult *= 10;
-    //}
+    std::cin >> n;
     int a;
-    std::cin >> n >> a;
+    std::cin >> a;
     int copy = a;
     int inp;
-    mult = 1;
     int b = 0;
     for (int i = 1; i <= n; i++) {
         inp = copy % 10;
         copy = (copy - inp) / 10;
-        b += mult * inp;
-        mult *= 10;
+        b = b * 10 + inp;
     }
-    std::cout << b << std::endl;
+    if (b == a)
+        std::cout << 1 << std::endl;
+    else
+        std::cout << b << std::endl;
     return 0;
 }
